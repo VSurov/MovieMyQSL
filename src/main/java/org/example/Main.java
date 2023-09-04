143c381b8d8721eb4132f118cb7c5da0679b9da7
VSurov <126866785+VSurov@users.noreply.github.com>
2023-09-04 21:26:55 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
