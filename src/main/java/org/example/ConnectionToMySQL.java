2738a0b2c3c2c3da07fc12e5bf7e25d68766a7ca
VSurov <vovasurov28@gmail.com>
2023-09-26 22:43:29 +0300

    add new class

---
+++ src/main/java/org/example/ConnectionToMySQL.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.sql.*;
+
+public class ConnectionToMySQL {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbmovie";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private Connection connection;
+
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
---
+++ src/main/java/org/example/Movie.java
@@ -0,0 +1,87 @@
+package org.example;
+
+public class Movie {
+    private int id;
+    private String name;
+    private String genre;
+    private int year;
+    private int rating;
+
+    public Movie() {
+    }
+
+    public Movie(String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public Movie(String name, String genre, int year) {
+        this.name = name;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public Movie(int id, String name, String genre, int year) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Movie(int id, String name, String genre, int year, int rating) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{id: " + id +
+                " Name: " + name +
+                ", genre: " + genre +
+                ", year: " + year + ",rating: "+rating+"}";
+    }
+
+}
